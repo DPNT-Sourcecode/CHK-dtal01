b26685c504725d59a6d8d14a0acad81204ed346d
sbx_user1051 <sbx_user1051@169.254.53.29>
2024-10-24 15:25:15 +0000

    Thu Oct 24 15:25:15 UTC 2024

--- lib/solutions/CHK/loader.py
+++ lib/solutions/CHK/loader.py
@@ -16,6 +16,8 @@ def load_offers(offers: list[str], price) -> dict:
             case [n_item, "get", "one", target, "free"]:
                 n, sku = n_item[0], n_item[1:]
                 if target == sku:
+                    logger.info(f"found freebie {sku} in {offer}, {n_item}")
+                    logger.info(f"hence offer for {int(n)} {sku} is {price * int(n)}")
                     parsed_offers[int(n)] = {"price": price * int(n)}
                 else:
                     parsed_offers[int(n)] = {"freebie": target}
@@ -55,6 +57,7 @@ def load_items(filepath: str):
         except ValueError as e:
             logger.debug(f'unable to parse line "{line.strip()}"')
             continue
+
 
 
 
