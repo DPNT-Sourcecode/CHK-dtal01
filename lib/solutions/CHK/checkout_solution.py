20b566a1cc6eadacd64d1e3ad22904eef9ad4a47
sbx_user1051 <sbx_user1051@169.254.53.29>
2024-10-24 13:21:04 +0000

    Thu Oct 24 13:21:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,10 @@ PRICES: dict = {
     "C": 20,
     "D": 15,
 }
+BUNDLES = {
+    "A": {"qty": 3, "price": 130},
+    "B": {"qty": 2, "price": 45},
+}
 
 
 def get_items(skus: str) -> list[str]:
@@ -26,5 +30,18 @@ def checkout(skus: str) -> int:
     for item, count in counts.items():
         if item not in PRICES:
             return -1
+
+        regular_price = PRICES[item]
+        bundled, remaining = 0, count
+
+        if item in BUNDLES:
+            bundle_size = BUNDLES[item]["qty"]
+            bundle_price = BUNDLES[item]["price"]
+
+            bundled, remaining = (
+                count // bundle_size,
+                count % bundle_size,
+            )
+
 
 
