b4d6553256737c4f326c779e1b44e02390dd81bd
sbx_user1051 <sbx_user1051@169.254.53.29>
2024-10-24 13:27:04 +0000

    Thu Oct 24 13:27:04 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,2 +1,17 @@
-def test_single_item():
-    assert 1 == 1
+import pytest
+
+from solutions.CHK import checkout_solutions
+
+
+@pytest.mark.parametrize("skus,total", [("A", 50), ("A A", 100), ("AA", 100)])
+def test_single_sku_total(skus, total):
+    assert checkout_solutions.checkout(skus) == total
+
+
+@pytest.mark.parametrize(
+    "skus,total",
+    [("AAA", 130), ("AAAA", 130 + 50), ("A" * 6, 130 * 2), ("A" * 7, 130 * 2 + 50)],
+)
+def test_single_sku_bundles(skus, total):
+    assert checkout_solutions.checkout(skus) == total
+
